5ce7aadbdc068bf393fbef44ad91db09c815149e
Jack Penn <jacktpenn@gmail.com>
2025-04-14 16:41:46 -0400

    Updated Project

---
+++ pakudex.py
@@ -0,0 +1,36 @@
+from gc import get_stats
+
+from pakuri import Pakuri
+
+class Pakudex:
+    def __init__(self, capacity = 20):
+        self.capacity = capacity
+        self.speciesList = []
+    def get_size(self):
+        return len(self.speciesList)
+    def get_capacity(self):
+        return self.capacity
+    def get_species_array(self):
+        if(self.get_size() == 0):
+            return None
+        return self.speciesList
+    def get_stats(self, species: str):
+        try:
+            p: Pakuri = next(p for p in self.speciesList if p.get_species() == species)
+            return [p.attack, p.defense, p.speed]
+        except StopIteration:
+            return None
+    def sort_pakuri(self):
+        self.speciesList.sort(key=lambda p: p.get_species)
+    def add_pakuri(self, species: str):
+        if(self.get_size() < self.capacity and self.get_stats(species) == None):
+            self.speciesList.append(Pakuri(species))
+            return True
+        return False
+    def evolve_species(self, species: str):
+        try:
+            p: Pakuri = next(p for p in self.speciesList if p.get_species() == species)
+            p.evolve()
+            return True
+        except StopIteration:
+            return False
---
+++ pakuri_program.py
@@ -0,0 +1,83 @@
+from pakudex import Pakudex
+
+pakudex: Pakudex
+class MenuOption:
+    def __init__(self, name: str, function = lambda : print("Function Not Implemented Yet")):
+        self.name = name
+        self.function = function
+
+def listPakuri():
+    global pakudex
+    pakuris = pakudex.get_species_array()
+    if(pakuris == None):
+        print("No Pakuri in Pakudex yet!")
+    else:
+        print("Pakuri In Pakudex:")
+        for i in range(len(pakuris)):
+            print(str(i+1) + ". " + pakuris[i].get_species())
+
+def showPakuri():
+    speciesName: str = input("Enter the name of the species to display: ")
+    stats = pakudex.get_stats(speciesName)
+    if(stats == None):
+        print("Error: No such Pakuri!")
+    else:
+        print("\nSpecies: " + speciesName)
+        print("Attack: " + str(stats[0]))
+        print("Defense: " + str(stats[1]))
+        print("Speed: " + str(stats[2]))
+def addPakuri():
+    if (pakudex.get_size() == pakudex.get_capacity()):
+        print("Error: Pakudex is full!")
+        return
+    speciesName: str = input("Enter the name of the species to add: ")
+    if(pakudex.add_pakuri(speciesName)):
+        print("Pakuri species " + speciesName + " successfully added!")
+    else:
+        if(pakudex.get_stats(speciesName) != None):
+            print("Error: Pakudex already contains this species!")
+def evolvePakuri():
+    speciesName: str = input("Enter the name of the species to evolve: ")
+    if(pakudex.evolve_species(speciesName)):
+        print(speciesName + " has evolved!")
+    else:
+        print("Error: No such Pakuri!")
+def sortPakuri():
+    pakudex.sort_pakuri()
+    print("Pakuri have been sorted!")
+
+if __name__ == '__main__':
+    print('Welcome to Pakudex: Tracker Extraordinaire!')
+    maxCapacity = 0
+    while(maxCapacity < 1 ):
+        try:
+            maxCapacity: int = int(input("Enter max capacity of the Pakudex: "))
+        except ValueError:
+            None
+        print("Please enter a valid size.")
+    print("The Pakudex can hold " + str(maxCapacity) + " species of Pakuri.")
+
+    pakudex = Pakudex(maxCapacity)
+
+    Menu = [MenuOption("List Pakuri", listPakuri),
+            MenuOption("Show Pakuri", showPakuri),
+            MenuOption("Add Pakuri", addPakuri),
+            MenuOption("Evolve Pakuri", evolvePakuri),
+            MenuOption("Sort Pakuri", sortPakuri),
+            MenuOption("Exit", lambda *args: None)]
+    selectedOption = 1
+    while (selectedOption != 5):
+        print("\nPakudex Main Menu")
+        print("-----------------")
+        for i in range(len(Menu)):
+            print(str(i+1) + ". " + Menu[i].name)
+
+        try:
+            selectedOption = int(input("\nWhat would you like to do? "))-1
+        except ValueError:
+            continue
+        if(selectedOption in range(0, len(Menu))):
+            Menu[selectedOption].function()
+        else:
+            print("Unrecognized menu selection!")
+    print("Thanks for using Pakudex! Bye!")
